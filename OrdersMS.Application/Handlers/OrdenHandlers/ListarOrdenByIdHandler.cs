f52a8fc736a2f40a9070641a155d254f283fc98d
Gamartinez25 <gamartinez.21@est.ucab.edu.ve>
2025-01-09 14:23:04 -0400

    Correcion[Req5.2.2]:Se corrigio en nombre del endpoint de orderPorId y el orden en el contructor

--- OrdersMS.Application/Handlers/OrdenHandlers/ListarOrdenByIdHandler.cs
+++ OrdersMS.Application/Handlers/OrdenHandlers/ListarOrdenByIdHandler.cs
@@ -7,46 +7,43 @@ using OrdersMS.Domain.Services;
 
 namespace OrdersMS.Application.Handlers.OrdenHandlers
 {
-    public class ListarOrdenesFinalizadasHandler : IRequestHandler<ListarOrdenesFinalizadasQuery, List<OrdenesFinalizadasDto>>
+    public class ListarOrdenByIdHandler : IRequestHandler<ListarOrdenByIdQuery, OrdenByIdDto>
     {
         private readonly IOrdenRepository OrdenRepository;
         private readonly IGoogleService GoogleService;
         
-        public ListarOrdenesFinalizadasHandler(IOrdenRepository ordenRepository, IGoogleService googleService)
+        public ListarOrdenByIdHandler(IOrdenRepository ordenRepository, IGoogleService googleService)
         {
             OrdenRepository = ordenRepository;
             GoogleService = googleService;
         }
 
-        public async Task<List<OrdenesFinalizadasDto>> Handle(ListarOrdenesFinalizadasQuery request, CancellationToken cancellationToken)
+        public async Task<OrdenByIdDto> Handle(ListarOrdenByIdQuery request, CancellationToken cancellationToken)
         {
             var ordenes = await OrdenRepository.GetAllOrdenAsync();
-            var ordenesPorGruero = ordenes.Where(x => x.Vehiculo == request.IdVehiculo);
-            List<OrdenesFinalizadasDto> ordenesFinalizadas = new List<OrdenesFinalizadasDto>();
-            foreach (var orden in ordenesPorGruero)
-            {
+            var orden = ordenes.Where(x => x.Id == request.IdOrden).First();
+            OrdenByIdDto ordenFinalizada;
+           
                 var estatus = await OrdenRepository.GetEstadoOrdenByIdOrdenAsync(orden.Id);
                 var polizaAsegurado = await OrdenRepository.GetPolizaAseguradoById(orden.PolizaAseguradoId);
-                if (estatus.EstadoActual == "Finalizado" || estatus.EstadoActual == "Pagado")
-                {
                     var (latOrigen, lonOrigen) = ObtenerCoordenadas.SepararCoordenadas(orden.DireccionOrigen);
                     var (latDestino, lonDestino) = ObtenerCoordenadas.SepararCoordenadas(orden.DireccionDestino);
                     var direccionOrigen = await GoogleService.GetDirecction(latOrigen, lonOrigen);
                     var direccionDestino = await GoogleService.GetDirecction(latDestino, lonDestino);
-                    var ordenFinalizada = new OrdenesFinalizadasDto(orden.Id,
+                    ordenFinalizada = new OrdenByIdDto(orden.Id,
                                                                         orden.NumeroFactura,
                                                                         orden.NombreDenunciante,
+                                                                        
                                                                         orden.Fecha.ToString(),
                                                                         direccionOrigen,
                                                                         direccionDestino,
                                                                         "#" + polizaAsegurado.Placa + ", " + polizaAsegurado.Marca + " " + polizaAsegurado.Modelo + ", " + polizaAsegurado.Color + ", " + polizaAsegurado.Anio + "."
                                                                         , orden.DetallesIncidente, orden.CostoTotal, orden.CostoServiciosAdicionales.Value, estatus.EstadoActual);
 
-                    ordenesFinalizadas.Add(ordenFinalizada);
-                }
+                return ordenFinalizada;
 
-            }
-            return ordenesFinalizadas;
+            
+            
         }
     }
 }
