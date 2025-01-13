42ff63be666260120ac67050fc74386ec9091740
Gamartinez25 <gamartinez.21@est.ucab.edu.ve>
2025-01-13 17:28:53 -0400

    Test-Exceptions

---
+++ OrdersMs.Test/Application/Exceptions/TestNotFoundOrderException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrdersMS.Application.Exceptions;
+
+namespace OrdersMs.Test.Application.Exceptions
+{
+    public class TestNotFoundOrderException
+    {
+        [Fact]
+        public void Constructor_ShouldSetMessage()
+        {
+            // Arrange
+            string expectedMessage = "Order not found";
+
+            // Act
+            var exception = new NotFoundOrderException(expectedMessage);
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+    }
+}
