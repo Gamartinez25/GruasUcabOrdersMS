f4fe7a3dc2e0b76f568059417f62b4658afce3e0
Gamartinez25 <gamartinez.21@est.ucab.edu.ve>
2025-01-13 17:09:45 -0400

    Correccion en la inicializacion de los validators

--- OrdersMS.Application/Handlers/CostoAdicionalHandlers/ModificarCostoAdicionalHandler.cs
+++ OrdersMS.Application/Handlers/CostoAdicionalHandlers/ModificarCostoAdicionalHandler.cs
@@ -4,6 +4,7 @@ using MediatR;
 using OrdersMS.Application.Commands.CostoAdicionalCommands;
 using OrdersMS.Application.Dtos.CostoAdicionalDtos;
 using OrdersMS.Application.Exceptions;
+using OrdersMS.Application.Validators.CostoAdicionalValidators;
 using OrdersMS.Core.Repositories;
 using OrdersMS.Domain.Entities;
 
@@ -12,18 +13,15 @@ namespace OrdersMS.Application.Handlers.CostoAdicionalHandlers
     public class ModificarCostoAdicionalHandler : IRequestHandler<ModificarCostoAdicionalCommand>
     {
         private readonly IMapper Mapper;
-        private readonly IValidator<ModificarCostoAdicionalDto> Validator;
         private readonly ICostoAdicionalRepository CostoAdicionalRepository;
 
-        public ModificarCostoAdicionalHandler(IMapper mapper, ICostoAdicionalRepository costoAdicionalRepository, IValidator<ModificarCostoAdicionalDto> validator)
+        public ModificarCostoAdicionalHandler(IMapper mapper, ICostoAdicionalRepository costoAdicionalRepository)
         {
             Mapper = mapper;
             CostoAdicionalRepository = costoAdicionalRepository;
-            Validator = validator;
         }
         public async Task Handle(ModificarCostoAdicionalCommand request, CancellationToken cancellationToken)
         {
-            // Validator.ValidateAndThrow(request.CostoAdicionalDto);
             await ValidarCostoAdicional(request);
             var existingCostoAdicional = await CostoAdicionalRepository.GetCostoAdicionalByIdAsync(request.Id);
             var nuevoCostoAdicional = Mapper.Map<OrdenCostoAdicional>((existingCostoAdicional, request.CostoAdicionalDto));
@@ -31,7 +29,8 @@ namespace OrdersMS.Application.Handlers.CostoAdicionalHandlers
         }
         private async Task ValidarCostoAdicional(ModificarCostoAdicionalCommand request)
         {
-            Validator.ValidateAndThrow(request.CostoAdicionalDto);
+            var validator=new ModificarCostoAdicionalValidator();
+            validator.ValidateAndThrow(request.CostoAdicionalDto);
             if (request.Id != request.CostoAdicionalDto.Id) { throw new IdsCostoAdicionalNoCoincidenException("Los ids del dto y de la peticion no coinciden"); }
             var existingCostoAdicional = await CostoAdicionalRepository.GetCostoAdicionalByIdAsync(request.Id);
             if (existingCostoAdicional.Estatus!= "Por Aprobar") { throw new InvalidAdditionalCostStateException("No se puede modificar un costo adicional que no está en estado Por Aprobar"); }
